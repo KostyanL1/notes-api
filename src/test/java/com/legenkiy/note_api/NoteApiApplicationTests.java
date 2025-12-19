1a1e7c8644e270f8a8ffa7bebe56634ae339863b
Костя Легенький <kostyantim.l05@gmail.com>
2025-12-19 21:21:32 +0200

    Initial commit.

---
+++ src/test/java/com/legenkiy/note_api/NoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legenkiy.note_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
