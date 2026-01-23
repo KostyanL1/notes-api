459f9eb8ad14d87c2ed90fc16596d38c0cb858a6
Костя Легенький <kostyantim.l05@gmail.com>
2026-01-23 17:43:21 +0200

    Completed NoteController.

--- src/main/java/com/legenkiy/note_api/controllers/NoteController.java
+++ src/main/java/com/legenkiy/note_api/controllers/NoteController.java
@@ -4,14 +4,11 @@ package com.legenkiy.note_api.controllers;
 import com.legenkiy.note_api.dto.NoteDto;
 import com.legenkiy.note_api.model.Note;
 import com.legenkiy.note_api.service.api.NoteService;
-import com.legenkiy.note_api.service.api.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jmx.export.metadata.ManagedOperation;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.plaf.PanelUI;
 import java.util.List;
 import java.util.Map;
 
@@ -29,27 +26,43 @@ public class NoteController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Note> getNoteById(@PathVariable("id") long id){
+    public ResponseEntity<Note> getNoteById(@PathVariable("id") long id) {
         return ResponseEntity.ok(noteService.findById(id));
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Map<String, Long>> saveNote(@RequestBody NoteDto noteDto){
+    public ResponseEntity<Map<String, Long>> saveNote(@RequestBody NoteDto noteDto) {
         long newNoteId = noteService.save(noteDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                        .body(
-                               Map.of(
-                                       "noteId", newNoteId
-                               )
-                        );
+                .body(
+                        Map.of(
+                                "noteId", newNoteId
+                        )
+                );
     }
 
-
     @PatchMapping("/update/{id}")
-    public ResponseEntity<Map<String, String>> updateNote(@RequestBody NoteDto noteDto, @PathVariable("id") long id){
+    public ResponseEntity<Map<String, Long>> updateNote(@RequestBody NoteDto noteDto, @PathVariable("id") long id) {
+        long updatedNoteId = noteService.update(noteDto, id);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(
+                        Map.of(
+                                "noteId", updatedNoteId
+                        )
+                );
+    }
 
-
+    @PatchMapping("/delete/{id}")
+    public ResponseEntity<Map<String, String>> deleteNote(@PathVariable("id") long id){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(
+                        Map.of(
+                                "message", "Note was deleted"
+                        )
+                );
     }
 
 }
