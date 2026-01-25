793c637ca8d3552aebae6c8156074edba528a75e
Костя Легенький <kostyantim.l05@gmail.com>
2026-01-25 20:26:27 +0200

    connected filter to sec cfg

--- src/main/java/com/legenkiy/note_api/security/SecurityCfg.java
+++ src/main/java/com/legenkiy/note_api/security/SecurityCfg.java
@@ -1,6 +1,8 @@
 package com.legenkiy.note_api.security;
 
 
+import com.legenkiy.note_api.security.filter.JwtAuthenticateFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,10 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityCfg {
+
+    private final JwtAuthenticateFilter jwtAuthenticateFilter;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration){
@@ -24,6 +30,7 @@ public class SecurityCfg {
         return httpSecurity
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(csrf -> csrf.disable())
+                .addFilterBefore(jwtAuthenticateFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(
                         http -> http
                                 .requestMatchers("/auth/**").anonymous()
