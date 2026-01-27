89014fa927d4e93e4812221894bbf1a05fd60212
Костя Легенький <kostyantim.l05@gmail.com>
2026-01-28 00:38:43 +0200

    Fixed filter form work with Bearer to cookie

--- src/main/java/com/legenkiy/note_api/security/filter/JwtAuthenticateFilter.java
+++ src/main/java/com/legenkiy/note_api/security/filter/JwtAuthenticateFilter.java
@@ -1,5 +1,6 @@
 package com.legenkiy.note_api.security.filter;
 
+import com.legenkiy.note_api.service.api.CookieService;
 import com.legenkiy.note_api.service.api.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -21,17 +22,23 @@ public class JwtAuthenticateFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final CookieService cookieService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String bearerToken = request.getHeader("Authorization");
-        if (bearerToken == null || !bearerToken.startsWith("Bearer ")) {
+        if (SecurityContextHolder.getContext().getAuthentication() != null){
+            filterChain.doFilter(request, response);
+            return;
+
+        }
+
+        String accessToken = cookieService.extractTokenFromCookie("accessToken", request);
+        if (accessToken == null ) {
             filterChain.doFilter(request, response);
             return;
         }
-        String token = bearerToken.substring(7);
-        UserDetails userDetails = userDetailsService.loadUserByUsername(jwtService.extractUsername(token));
-        if (!jwtService.isTokenValid(token, userDetails)) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(jwtService.extractUsername(accessToken));
+        if (!jwtService.isTokenValid(accessToken, userDetails)) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -40,6 +47,7 @@ public class JwtAuthenticateFilter extends OncePerRequestFilter {
                 null,
                 userDetails.getAuthorities()
         );
+
         SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
         filterChain.doFilter(request, response);
 
